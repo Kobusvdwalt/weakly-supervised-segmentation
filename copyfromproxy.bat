6add148220745e07f776a4fa26a7491e7f61302c
kobusvdwalt9 <kobus.vanderwalt@tradesafe.co.za>
2020-09-14 12:12:06 +0200

    add chpc job

--- copyfromproxy.bat
+++ copyfromproxy.bat
@@ -9,3 +9,7 @@ scp -P 443 -i details.pem -r ubuntu@ec2-13-244-167-52.af-south-1.compute.amazona
 :: # Copy specific weight file
 :: # Step1: rsync -chavzP --stats pjvanderwalt@146.141.21.100:~/weakly-supervised-segmentation/models/checkpoints/multiclass_up.pt ./ 
 :: # Step2: scp -P 443 -i details.pem -r ubuntu@ec2-13-244-167-52.af-south-1.compute.amazonaws.com:~/multiclass_up.pt ./
+
+
+:: # CHPC interactive session
+:: qsub -I -P CSCI1340 -q serial -l select=1:ncpus=1:mpiprocs=1:nodetype=haswell_reg
